a4c141f03bf4376ad3fbd033c9d1b2ce64d3147c
shared.repo.z <shared.repo.z@gmail.com>
2025-12-16 11:05:14 +0900

    update code

---
+++ workspaces/java-basic/Ex07OOP3/src/AppH.java
@@ -0,0 +1,23 @@
+// 1. 인터페이스 정의
+interface TheInterface4 {
+	void im(); // -> public abstract void im();
+	// void im(); // 4. 인터페이스에 새 기능 추가 -> 기존에 이 인터페이스를 구현하는 모든 클래스에 오류 발생	
+	default void im2() {
+		System.out.println("TheInterface4.default method im2()");
+	}
+}
+// 2. 인터페이스 구현 클래스 정의
+class TheImplement4 implements TheInterface4 {
+	@Override
+	public void im() {
+		System.out.println("TheImplement4.im()");
+	}	
+}
+public class AppH {
+	public static void main(String[] args) {		
+		// 3. 인터페이스 사용
+		TheInterface4 i = new TheImplement4();
+		i.im();
+		i.im2(); // 인터페이스의 default 메서드는 재정의 없이 사용 가능 
+	}
+}
